f07e2638d69894e49ffcac5cd11ae394b1a6123d
alphaecho <alphaecho42@gmail.com>
2021-09-27 09:13:52 +0300

    Initial insert of the basic code and example

---
+++ code/lifxlan.h
@@ -0,0 +1,283 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <cstring>
+#include <stdlib.h>
+#include <math.h>
+using namespace std;
+//Based on documentations by lifx https://lan.developer.lifx.com/docs/encoding-a-packet
+#pragma pack(push, 1)
+#define MESSAGE_SOURCE 0x42424242
+typedef struct {
+  /* frame */
+  uint16_t size;
+  uint16_t protocol:12;
+  uint16_t  addressable:1;
+  uint16_t  tagged:1;
+  uint16_t  origin:2;
+  uint32_t source;
+  /* frame address */
+  uint8_t  target[8];
+  uint8_t  reserved[6];
+  uint8_t  res_required:1;
+  uint8_t  ack_required:1;
+  uint8_t  :6;
+  uint8_t  sequence;
+  /* protocol header */
+  uint64_t :64;
+  uint16_t type;
+  uint16_t :16;
+  /* variable length payload follows */
+} lx_protocol_header_t;
+
+typedef struct {
+    lx_protocol_header_t header;
+    uint8_t  reserved;
+    uint16_t hue;
+    uint16_t saturation;
+    uint16_t brightness;
+    uint16_t kelvin;
+    uint16_t duration;
+} tx_lx_setcolor_Packet102;
+
+typedef struct {
+    lx_protocol_header_t header;
+    uint16_t power;
+}tx_lx_setpower_Packet21;
+
+//RX
+typedef struct {
+  lx_protocol_header_t header;
+  uint8_t service; //1 is UDP; others are reserved and are not for us
+  uint32_t portOfService; //Uusally 56700 but don't assume this is allways the case
+}rx_stateService_Packet003;
+
+typedef struct {
+    lx_protocol_header_t header;
+    uint16_t power;
+}rx_statePower_Packet22;
+
+
+
+typedef struct {
+  lx_protocol_header_t header;
+  uint16_t hue;
+  uint16_t saturation;
+  uint16_t brightness;
+  uint16_t kelvin;
+  uint8_t reserved6[2];
+  uint16_t power;
+  uint8_t label[32];
+  uint8_t reserved7[8];
+}rx_lightState_Packet107;
+
+typedef struct {
+    lx_protocol_header_t header;
+    uint32_t vendor;
+    uint32_t product;
+    char reserved[4];
+}rx_stateVersion_Packet33;
+
+
+
+typedef struct {
+    uint16_t hue;
+    float saturation;
+    float brightness;
+    uint16_t kelvin;
+    bool power;
+}light_parameters;
+
+int build_tx_message_base(char* raw_message, uint16_t size, uint16_t type = 2)
+{
+    lx_protocol_header_t* message = (lx_protocol_header_t*)raw_message;
+    memset(message, 0, sizeof(lx_protocol_header_t));
+    message->type = 2;
+    message->protocol = 1024;
+    message->size = sizeof(lx_protocol_header_t);
+    message->source = MESSAGE_SOURCE;
+    message->addressable = 1;
+    message->tagged = 1;
+    message->res_required = true;
+    message->ack_required = false;
+    message->type = type;
+    return message->size;
+}
+
+//GET MESSAGE
+int build_discovery_message(char* raw_message, uint16_t size)
+{
+    int len = build_tx_message_base(raw_message, size,2);
+    return len;
+}
+
+int build_get_power_message(char* raw_message, uint16_t size)
+{
+    int len = build_tx_message_base(raw_message, size,20);
+    return len;
+}
+
+int build_get_color_message(char* raw_message, uint16_t size)
+{
+    int len = build_tx_message_base(raw_message, size,101);
+    return len;
+}
+
+int build_get_version_message(char* raw_message, uint16_t size)
+{
+    int len = build_tx_message_base(raw_message, size,32);
+    return len;
+}
+//SET MESSAGE
+int build_set_color_message(char* raw_message, uint16_t size, light_parameters light_params)
+{
+    int len = sizeof(tx_lx_setcolor_Packet102);
+    build_tx_message_base(raw_message, size, 102);
+    tx_lx_setcolor_Packet102* message = (tx_lx_setcolor_Packet102*)raw_message;
+    uint16_t hue = int(round(0x10000 * light_params.hue) / 360) % 0x10000;
+    uint16_t saturation = int(round(0xFFFF * light_params.saturation));
+    uint16_t brightness = int(round(0xFFFF * light_params.brightness));
+    message->duration = 1;
+    message->hue = hue;
+    message->brightness = brightness;
+    message->saturation = saturation;
+    message->kelvin = light_params.kelvin;
+    message->header.size = len;
+    return len;
+}
+
+int build_set_power_message(char* raw_message, uint16_t size, bool power)
+{
+    
+    int len = sizeof(tx_lx_setpower_Packet21);
+    build_tx_message_base(raw_message, size, 21);
+    tx_lx_setpower_Packet21* message = (tx_lx_setpower_Packet21*)raw_message;
+    message->power = power ? 0xFFFF : 0;
+    return len;
+}
+
+
+
+
+
+int decode_lifx_header(char* raw_message, uint16_t size)
+{
+    lx_protocol_header_t* message = (lx_protocol_header_t*)raw_message;
+    printf("-------------------\n");
+    printf("Target: ");
+    for (int i = 0; i < 8; i++)
+    {
+        printf("%X", message->target[i]);
+    }
+    printf("\n");
+    printf("Message type = %d\n", message->type);
+    printf("Message Len = %d\n", message->size);
+    return message->type;
+}
+
+bool get_lifx_device_id(char* raw_message, uint16_t size, char* id_buf, uint16_t id_buf_len)
+{
+    memset(id_buf, 0, id_buf_len);
+    bool ret_value = false;
+    if (id_buf_len == 8)
+    {
+        lx_protocol_header_t* message = (lx_protocol_header_t*)raw_message;
+        for (int i = 0; i < 8; i++)
+        {
+            id_buf[i] = message->target[i];
+        }
+        ret_value = true;
+    }
+    return ret_value;
+}
+
+void decode_lifx_statePower(char* raw_message, uint16_t size)
+{
+    rx_statePower_Packet22* message = (rx_statePower_Packet22*)raw_message;
+    printf("power = %x\n", message->power);
+}
+
+
+void decode_lifx_stateService(char* raw_message, uint16_t size)
+{
+    rx_stateService_Packet003* message = (rx_stateService_Packet003*)raw_message;
+    printf("service = %d\n", message->service);
+    printf("port = %d\n", message->portOfService);
+}
+
+void decode_lifx_lightState(char* raw_message, uint16_t size)
+{
+    rx_lightState_Packet107* message = (rx_lightState_Packet107*)raw_message;
+    for (int i = 0; i < 32; i++)
+    {
+        printf("%c", message->label[i]);
+    }
+    light_parameters light_params;
+    light_params.hue = round((double(message->hue) * 360 / 0x10000)*100)/100; //Round to 2 decimal places
+    light_params.brightness = round((double(message->brightness) / 0xFFFF) * 10000) / 10000; //Round to 4 decimal places
+    light_params.saturation = round((double(message->saturation) / 0xFFFF) * 10000) / 10000; //Round to 4 decimal places
+    light_params.power = message->power != 0;
+    light_params.kelvin = message->kelvin;
+    
+    printf("\n");
+    printf("hue = %d\n", light_params.hue);
+    printf("saturation = %.4f\n", light_params.saturation);
+    printf("brightness = %.4f\n", light_params.brightness);
+    printf("kelvin = %d\n", light_params.kelvin);
+    printf("power = %s\n", light_params.power ? "TRUE" : "FALSE");
+}
+
+void decode_lifx_stateVersion(char* raw_message, uint16_t size)
+{
+    rx_stateVersion_Packet33* message = (rx_stateVersion_Packet33*)raw_message;
+    printf("vendor: %d\n", message->vendor);
+    printf("id: %d ", message->product);
+    switch (message->product)
+    {
+    case 27:
+        printf("LIFX A19");
+        break;
+    case 60:
+        printf("LIFX Mini White to Warm");
+        break;
+    case 61:
+        printf("LIFX MINI White");
+        break;
+    default:
+        printf("Uncategorized");
+        break;
+    }
+    printf("\n");
+
+
+}
+
+void  decode_lifx_message(char* raw_message, uint16_t size)
+{
+  int type = decode_lifx_header(raw_message, size);
+  void (*decode_func)(char*, uint16_t) = nullptr;
+  //StateService - packet3
+  switch (type)
+  {
+  case 3:
+      decode_func = decode_lifx_stateService;
+      break;
+  case 22:
+      decode_func = decode_lifx_statePower;
+      break;
+  case 107:
+      decode_func = decode_lifx_lightState;
+      break;
+  case 33:
+      decode_func = decode_lifx_stateVersion;
+  }
+
+  if (decode_func != nullptr)
+  {
+      decode_func(raw_message, size);
+  }
+  
+   //light
+   //Light State packet_107
+}
+#pragma pack(show)
+#pragma pack(pop)
---
+++ examples/win/Main.cpp
@@ -0,0 +1,208 @@
+using namespace std;
+#include "lifxlan.h"
+
+/*
+	Simple udp client
+*/
+#include<stdio.h>
+#include<winsock2.h>
+#include<stdlib.h>
+#include <iostream>
+//#include <vector>
+#include <map>
+#pragma comment(lib,"ws2_32.lib") //Winsock Library
+
+#define BUFLEN 512	//Max length of buffer
+#define MYPORT 56700    // the port users will be connecting to
+using namespace std;
+
+
+class CLightInfo
+{
+public:
+	char id[9];
+	int last_ip_byte;
+	CLightInfo()
+	{
+		memset(id, 0, 8);
+	}
+	
+	~CLightInfo()
+	{
+		memset(id, 0, 8);
+	}
+};
+
+
+
+int run_2()
+{
+	WSADATA wsaData;
+	
+	std::map<uint32_t, CLightInfo*> lights_container;
+
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+	SOCKET sock;
+	sock = socket(AF_INET, SOCK_DGRAM, 0);
+	char broadcast = '1';
+	if (setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &broadcast, sizeof(broadcast)) < 0)
+	{
+		cout << "Error in setting Broadcast option";
+		closesocket(sock);
+		return 0;
+	}
+
+	
+	
+	struct sockaddr_in Recv_addr;
+	struct sockaddr_in Sender_addr;
+	int len = sizeof(struct sockaddr_in);
+	char sendMSG[BUFLEN];
+	int len2 = build_discovery_message(sendMSG, 100);
+	char recvbuff[BUFLEN] = "";
+	int recvbufflen = BUFLEN;
+	Recv_addr.sin_family = AF_INET;
+	Recv_addr.sin_port = htons(MYPORT);
+
+	Recv_addr.sin_addr.s_addr = inet_addr("10.175.245.255");  // switch doesnt work
+
+	// setup a timeval structure for use in select later in the program
+	int timeout = 1000;
+	timeval time_to_wait;
+	time_to_wait.tv_sec = static_cast<long>(timeout / 1000);
+	time_to_wait.tv_usec = static_cast<long>((timeout % 1000) * 1000);
+	fd_set read_set;
+	// initialize read_set
+	FD_ZERO(&read_set);
+	// add the listening socket to read_set
+	FD_SET(sock, &read_set);
+	///
+	sendto(sock, sendMSG,len2, 0, (sockaddr*)&Recv_addr, sizeof(Recv_addr));
+	Sleep(1000);
+
+	bool try_receive = true;
+	do
+	{
+		FD_ZERO(&read_set);
+		// add the listening socket to read_set
+		FD_SET(sock, &read_set);
+		int status = select(0, &read_set, 0, 0, &time_to_wait);
+		if (status == 0)
+		{
+			try_receive = false;
+		}
+		else
+		{
+			recvfrom(sock, recvbuff, recvbufflen, 0, (sockaddr*)&Sender_addr, &len);
+			auto ip_address = Sender_addr.sin_addr.S_un.S_addr;
+			auto ip_last_byte = Sender_addr.sin_addr.S_un.S_un_b.s_b4;
+			auto val = lights_container.find(ip_address);
+			if (val == lights_container.end())
+			{
+				CLightInfo* new_light_info = new CLightInfo();
+				printf("^^^^^^^^^^^^^^^^^^^^^^\n");
+				printf("message from ip: %d\n", ip_last_byte);
+				decode_lifx_message(recvbuff, recvbufflen);
+				get_lifx_device_id(recvbuff, recvbufflen, new_light_info->id, 8);
+				new_light_info->last_ip_byte = ip_last_byte;
+				lights_container[ip_address] = new_light_info;
+
+			}
+		}
+		
+		
+	} while (try_receive);
+	printf("=======================================");
+	for (auto& ip : lights_container) {
+		
+		Recv_addr.sin_addr.S_un.S_addr = ip.first;
+		int last_ip_byte = Recv_addr.sin_addr.S_un.S_un_b.s_b4;
+ 		cout << "****************" << endl;
+		cout << "Commands are sent to this IP: " << (int)last_ip_byte << endl;
+		/*
+		len2 = build_discovery_message(sendMSG, 100);
+		sendto(sock, sendMSG, len2, 0, (sockaddr*)&Recv_addr, sizeof(Recv_addr));
+		len2 = build_get_power_message(sendMSG, 100);
+		sendto(sock, sendMSG, len2, 0, (sockaddr*)&Recv_addr, sizeof(Recv_addr));
+		*/
+		//D073D52192BB00
+		if (last_ip_byte == 160)
+		{
+			light_parameters light_params;
+			/*
+			light_params.brightness = 1;
+			light_params.hue = 336;
+			light_params.saturation = 0;
+			light_params.power = true;
+			light_params.kelvin = 3500;
+			*/
+			light_params.brightness = 1;
+			light_params.hue = 300;
+			light_params.saturation = 0;
+			light_params.power = false;
+			light_params.kelvin = 3500;
+			//len2 = build_set_color_message(sendMSG, BUFLEN, light_params);
+			len2 = build_set_power_message(sendMSG, BUFLEN, true);
+			sendto(sock, sendMSG, len2, 0, (sockaddr*)&Recv_addr, sizeof(Recv_addr));
+			Sleep(1000);
+		}
+		len2 = build_get_version_message(sendMSG, 100);
+		sendto(sock, sendMSG, len2, 0, (sockaddr*)&Recv_addr, sizeof(Recv_addr));
+		len2 = build_get_color_message(sendMSG, 100);
+		sendto(sock, sendMSG, len2, 0, (sockaddr*)&Recv_addr, sizeof(Recv_addr));
+		Sleep(1000);
+		/*
+		len2 = build_get_color_message(sendMSG, 100);
+		sendto(sock, sendMSG, len2, 0, (sockaddr*)&Recv_addr, sizeof(Recv_addr));
+		len2 = build_get_color_message(sendMSG, 100);
+		sendto(sock, sendMSG, len2, 0, (sockaddr*)&Recv_addr, sizeof(Recv_addr));
+		*/
+		cout << "\nWaiting for replys...\n";
+		try_receive = true;
+		do
+		{
+			FD_ZERO(&read_set);
+			// add the listening socket to read_set
+			FD_SET(sock, &read_set);
+
+			int status = select(0, &read_set, 0, 0, &time_to_wait);
+			if (status == -1)
+			{
+				perror("Select fail Reson: ");
+				printf("Error in socket %d\n", WSAGetLastError());
+			}
+			if (status == 0)
+			{
+				try_receive = false;  
+							
+			}
+			else
+			{
+				recvfrom(sock, recvbuff, recvbufflen, 0, (sockaddr*)&Sender_addr, &len);
+				decode_lifx_message(recvbuff, BUFLEN);
+			}
+		} while (try_receive);
+
+		
+	
+		
+	}
+
+	
+
+	closesocket(sock);
+	WSACleanup();
+	
+	//Free allocated memory
+	for (auto& ip : lights_container) {
+
+		delete ip.second;
+	}
+	lights_container.clear();
+}
+
+int main()
+{
+	run_2();
+	//run_1();
+ }
